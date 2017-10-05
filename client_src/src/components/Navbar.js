49a017a4b84616b79efcf9522a01642007264bca
motosharpley <motosharpley@gmail.com>
2017-10-05 12:58:54 -0500

    react front end and production build

---
+++ client_src/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component{
+  render(){
+    return (
+      <div>
+        <nav className="blue darken-4">
+          <div className="nav-wrapper">
+            <a data-activates="main-menu" className="button-collapse show-on-large"><i className="fa fa-bars"></i></a>
+            <ul className="right hide-on-small-only">
+              <li><Link to="/"><i className="fa fa-users"></i> Meetups</Link></li>
+
+            </ul>
+            <ul className="side-nav" id="main-menu">
+              <li><Link to="/"><i className="fa fa-users"></i> Meetups</Link></li>
+              <li><Link to="/meetups/add"><i className="fa fa-plus"></i> Add Meetups</Link></li>
+              <li><Link to="/about"><i className="fa fa-question-circle"></i> About</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
